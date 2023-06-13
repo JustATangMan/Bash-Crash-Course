de8418d86ec96bc4c43247ba86e89ac0a450c62c
EC2 Default User <ec2-user@ip-172-31-83-21.ec2.internal>
2023-06-13 01:58:46 +0000

    first commit

---
+++ hot-folder/script2.sh
@@ -0,0 +1,32 @@
+#! /bin/bash 
+rm -r ./hot-folder
+mkdir ./hot-folder
+for i in $(find . -type d)
+do
+if [ $i != "./hot-folder" ]; then
+
+	echo "this is the folder: $i"
+	for myfile in $i/*;
+	do
+		if [ -f "$myfile" ]; then
+			check=$(grep -ni "apple" "$myfile")
+			if [ -z "$check" ]; then
+				echo "Empty"
+			else
+				echo "Found file"
+				name_f=$(basename $myfile)
+				cp "$myfile" ./hot-folder/"$name_f"
+				echo " " >> hot-folder/"$name_f"
+				echo "*******" >> hot-folder/"$name_f"
+				echo "$check" >> hot-folder/"$name_f"
+			fi
+		fi
+	done
+	echo "----------"
+	echo " "
+fi
+done
+
+ 
+*******
+12:			check=$(grep -ni "apple" "$myfile")
---
+++ script.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+mkdir ./hot-folder
+
+for i in *;
+do
+	if [ -f "$i" ]; then
+		linenum=$(grep -ni "apple" $i)
+		if [ -z "$linenum" ]; then
+			echo "Empty"
+		else
+			echo "found an apple"
+			cp "$i" ./hot-folder
+			echo " " >> hot-folder/"$i"
+			echo "$linenum" >> hot-folder/"$i"
+		fi
+	else
+	       	echo "$i is not a file"	
+	fi
+done
